32895f6be6ef5fc0fbfa2f516d768b5575da699c
JoshuaSiyame <josh2023decoded@gmail.com>
2023-11-21 21:14:46 +0300

    final Commits

--- main.cpp
+++ main.cpp
@@ -6,27 +6,37 @@
 using namespace std;
 using namespace cv;
 
-int main(){
-    cout << "*** Face Detection using OpenCV ***" <<endl;
+int main()
+{
+    cout << "*** Face Detection using OpenCV ***" << endl;
 
     // capture object
     VideoCapture lap_cam(0);
 
-    // check if camera is reading 
-    if(!lap_cam.isOpened()){
-        cout<<"Failed to get Camera Readings..."<<endl;
-        return -1;
-    }else{
-        cout<<"Getting Camera Readings Successfully"<<endl;
-    };
+    // prepare cascade object and load cascade classifier file
+    CascadeClassifier face_detection;
+    
+    // image object
+    Mat frames;    
 
-    Mat frames;
+    face_detection.load("D:\\C++\\OpenCV\\15_face_detection_cascadeclassifier\\haarcascade_frontalface_default.xml");
 
     while (true)
     {
         lap_cam.read(frames);
+
+        // create vector for a face
+        vector<Rect> faces;
+
+        face_detection.detectMultiScale(frames, faces, 1.3, 5);
+
+        // loop to go through the video
+        for( int i=0; i<faces.size(); i++){
+            rectangle(frames, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+        };
+
         namedWindow("Laptop Camera Reading...", WINDOW_FREERATIO);
         imshow("Laptop Camera Reading...", frames);
         waitKey(1);
-    };    
+    };
 };
